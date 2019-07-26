51461a8e6b28798d366e232f9e7b6b63d071e284
Assia Touati <assiatouati1@gmail.com>
2019-07-26 03:19:30 +0000

    Updates

---
+++ precache-manifest.ed8e27201dafc1b04ad470d8fd5bdd23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39f0cf5193628d6d0a28142e59703dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5011b9df92008b5cab33",
+    "url": "/static/css/main.4251cd27.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "5011b9df92008b5cab33",
+    "url": "/static/js/main.5796ce1c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "49b5a5d54584670d4047fcb7b2f60093",
+    "url": "/static/media/logo.49b5a5d5.svg"
+  }
+]);
